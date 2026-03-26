5e58442634cb1de097543ec2b9bd1b34f3bd864f
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-03-25 22:18:09 -0300

    atualiza

---
+++ 04-layout/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className="cont-app">
+    <header className={`cont-header`}>Nome/logo/links</header>
+    <main className={`cont-main`}>Aqui as coisas acontece e aparece</main>
+    <section>aleatoria</section>
+    <footer>rodapé</footer>
+   </div>
+  )
+}
+
+export default App
