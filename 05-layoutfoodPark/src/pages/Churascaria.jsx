cc3c08cf8cbea63694fc8238d0e62178941c7f03
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-04-06 22:15:07 -0300

    atualizaçao do food

--- 05-layoutfoodPark/src/pages/Churascaria.jsx
+++ 05-layoutfoodPark/src/pages/Churascaria.jsx
@@ -3,7 +3,22 @@ import './Paginas.css'
 function Churascaria() {
   return (
     <div className={'cont-pizza'}>
+      <section className='esquerda'>
         <h1>Pizza Ruth</h1>
+        🍕 Pizza Ruth 🍕
+
+Na Pizza Ruth, cada fatia conta uma história de sabor e tradição! Trabalhamos com ingredientes frescos, massas artesanais e aquele toque especial que transforma qualquer momento em uma experiência deliciosa.
+
+Seja para reunir a família, curtir com os amigos ou matar aquela vontade de pizza, aqui você encontra qualidade, carinho e muito sabor em cada pedido.
+
+🔥 Do clássico ao especial, a sua pizza favorita está aqui!
+📍 Pizza Ruth – o lugar onde o sabor é levado a sério.
+
+      </section>
+      <section className='direita'>
+    <img src="./public/pizza.png" className='imagempro' ></img>
+
+      </section>
         </div>
   )
 }
--- 05-layoutfoodPark/src/pages/Home.jsx
+++ 05-layoutfoodPark/src/pages/Home.jsx
@@ -2,24 +2,30 @@ import './Paginas.css'
 
 function Home() {
   return (
-    <div className={'cont-home'}>
-
-    <h1>Bem-vindo ao nosso foodtruck, onde o sabor não tem limites! 🚚🔥</h1>
-    <p>Aqui você encontra o melhor da comida de rua em um só lugar:
+    <div className={'cont-home '}>
+      <section className='esquerda'>
+        <h1 className={'titulo glass-effect' }>Bem-vindo ao nosso foodtruck, onde o sabor não tem limites! 🚚🔥</h1>
+    <p className={ 'paragrafo_1 glass-effect'} >Aqui você encontra o melhor da comida de rua em um só lugar:
       carnes suculentas direto da brasa, pastéis crocantes recheados na hora e pizzas irresistíveis com aquele toque especial.</p>
 
-    <p>Nossa missão é simples: entregar qualidade, sabor e uma experiência única a cada mordida.
+    <p className={'paragrafo_1 glass-effect'}>Nossa missão é simples: entregar qualidade, sabor e uma experiência única a cada mordida.
  Seja para matar a fome ou reunir os amigos, temos a opção perfeita pra você.</p>
 
-<div className="menu">
+<div className={'menu glass-effect'}>
         <span>🥩 Churrasco</span>
         <span>🥟 Pastel</span>
         <span>🍕 Pizza</span>
       </div>
 
-<p className="frase">
+<p className={'frase glass-effect'}>
         Porque aqui, cada parada é uma nova experiência de sabor!
       </p>
+      </section>
+      <section className='direita'>
+      <img src="./public/propaganda.png" className='imagempro' ></img>
+      </section>
+
+   
 
     </div>
   )
