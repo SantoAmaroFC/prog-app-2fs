1b88a800d2de568ab717f452f19a94af43d697f4
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-05-06 19:40:25 -0300

    cards inputs

--- 06-cards/src/App.jsx
+++ 06-cards/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
     {id: 3, nome: 'Genius', preco: 100, imagem: '🛸'}
   ])
 
+  const[inputImagem, setImpuImagem] = useState('')
+  const[inputNome, setImputNome] = useState('')
+  const[inputPreco, setImputPreco] = useState('')
+
   function cadastrar(){
     let produtoNovo =  {id: Date.now(), nome: 'Pogobal', preco: 10, imagem: '🪐'}
     setProdutos([...produtos , produtoNovo])
@@ -18,7 +22,34 @@ function App() {
   return (
     <div>
       <h1> cards de coisas</h1>
-      <buttton onClick={cadastrar}>+</buttton>
+      <div className="cont-form">
+      <h2>Cadastro das coisas</h2>
+        <div className="input-container">
+        <label htmlFor="">Imagem</label>
+        <input type="text"
+        value={inputImagem}
+        onChange={(e) => setImpuImagem(e.target.value)}
+        />
+        </div>
+
+        <div className="input-container">
+        <label htmlFor="">Nome</label>
+        <input type="text"
+        value={inputNome}
+        onChange={(e) => setImputNome(e.target.value)}
+        />
+        </div>
+
+        <div className="input-container">
+        <label htmlFor="">preço</label>
+        <input type="text"
+        value={inputPreco}
+        onChange={(e) => setImputPreco(e.target.value)}
+        />
+        </div>
+
+      <buttton onClick={cadastrar}>Cadastrar</buttton>
+      </div>
       <div className='listaCards'>
      {
        produtos.map((produto) => (
