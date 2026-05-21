c989967d3e6b7f274bf51045212f2d341346ad20
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-05-21 19:15:51 -0300

    toma

---
+++ 08-exerciciodecardsdescrever/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const[produtos, setProdutos] = useState([])
+  const[imagem, setImagem] = useState("")
+  const[nome, setNome] = useState("")
+  const[descricao, setDescricao] = useState("")
+
+    function cadastrar(){
+      let cadastrarProduto
+        id: setId
+        imagem:  setImagem
+        nome: setNome
+        descricao: setDescricao
+    }
+
+
+  return (
+
+
+
+
+
+
+    <div className="cont-main">
+    
+    
+    
+    </div>
+    
+  )
+}
+
+export default App
