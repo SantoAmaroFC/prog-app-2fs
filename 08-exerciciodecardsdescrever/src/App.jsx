60fa63dc3a235791031c76bda1826e6dfd85375a
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-05-21 21:33:23 -0300

    cadastroManual

--- 08-exerciciodecardsdescrever/src/App.jsx
+++ 08-exerciciodecardsdescrever/src/App.jsx
@@ -1,36 +1,92 @@
 import { useState } from 'react'
 import './App.css'
+import Contatinho from './components/Contatinho';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const[produtos, setProdutos] = useState([])
-  const[imagem, setImagem] = useState("")
-  const[nome, setNome] = useState("")
-  const[descricao, setDescricao] = useState("")
+  const[contatinhos, setContatinhos] = useState([])
+  const[inputFoto, setInputFoto] = useState('')
+  const[inputNome, setInputNome] = useState('')
+  const[inputArroba, setInputArroba] = useState('')
+  const[inputFone, setInputFone] = useState('')
 
-    function cadastrar(){
-      let cadastrarProduto
-        id: setId
-        imagem:  setImagem
-        nome: setNome
-        descricao: setDescricao
+    function cadastrarContatinho(){
+      const novoContatinho = {
+        id: Date.now(),
+        foto: inputFoto,
+        nome: inputNome,
+        arroba: inputArroba,
+        fone: inputFone
+      }
+      setContatinhos([novoContatinho ,...contatinhos])
+      console.log(contatinhos)
     }
+      function excluir(id){
+        setContatinhos(contatinhos.filter((c)=>c.id != id))
+      }
+
+    
 
 
   return (
-
-
-
-
-
-
-    <div className="cont-main">
+    <div className="cont-app">
+    <div className="form-cadastro">
+    <p>Cadastro de contatinhos</p>
     
-    
+    <div className="cont-input">
+
+    <label>Foto</label>
+    <input type="text"
+    value={inputFoto}
+    onChange={(e) => setInputFoto(e.target.value)}
+    />
     
     </div>
+
+    <div className="cont-input">
+
+    <label>Nome</label>
+    <input type="text"
+    value={inputNome}
+    onChange={(e) => setInputNome(e.target.value)}
+    />
+    
+    </div>
+
+    <div className="cont-input">
+
+    <label>Arroba</label>
+    <input type="text"
+    value={inputArroba}
+    onChange={(e) => setInputArroba(e.target.value)}
+    />
+    
+    </div>
+
+    <div className="cont-input">
+
+    <label>Fone</label>
+    <input type="text"
+    value={inputFone}
+    onChange={(e) => setInputFone(e.target.value)}
+    />
+    
+    </div>
+    <div className="cont-input">
+    <button onClick={cadastrarContatinho}>Cadastar</button>
+    </div>
+    </div>
+    
+    <div className="listaContatinho">
+    {contatinhos.map((contatinho) => (
+      
+      <Contatinho key={contatinho.id} c={contatinho} excluir={excluir} /> 
+      ))}
+
+    </div>
+      </div>
     
   )
+  
 }
 
 export default App
---
+++ 08-exerciciodecardsdescrever/src/components/Contatinho.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Contatinho({c, excluir}) {
+   const {foto, nome, arroba, fone, id} = c
+   
+    return (
+        <div className='cont-contatinho'>
+            <h2>{nome}</h2>
+            <p>{foto}</p>
+            <img src={foto} alt={foto} style={{width: '200px'}} />
+            <p>{arroba}</p>
+            <p>{fone}</p>
+            <button onClick={() => excluir(id)}>Excluir</button>
+
+        </div>
+    )
+    }
+
+
+export default Contatinho
